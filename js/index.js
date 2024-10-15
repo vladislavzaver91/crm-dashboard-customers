d8b21c65f99bde283dc75541bf3bb673e95c7e79
vladislavzaver91 <vladislavzaver91@gmail.com>
2024-10-15 15:32:10 +0300

    feat: fix index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,70 @@
-document.addEventListener('DOMContentLoaded', async function () {
-	const response = await fetch('../services/data.json');
-	const data = await response.json();
+document.addEventListener('DOMContentLoaded', function () {
+	const data = [
+		{
+			customerName: 'Jane Cooper',
+			company: 'Microsoft',
+			phoneNumber: '(225) 555-0118',
+			email: 'jane@microsoft.com',
+			country: 'United States',
+			status: 'Active',
+		},
+		{
+			customerName: 'Floyd Miles',
+			company: 'Yahoo',
+			phoneNumber: '(205) 555-0100',
+			email: 'floyd@yahoo.com',
+			country: 'Kiribati',
+			status: 'Inactive',
+		},
+		{
+			customerName: 'Ronald Richards',
+			company: 'Adobe',
+			phoneNumber: '(302) 555-0107',
+			email: 'ronald@adobe.com',
+			country: 'Israel',
+			status: 'Inactive',
+		},
+		{
+			customerName: 'Marvin McKinney',
+			company: 'Tesla',
+			phoneNumber: '(252) 555-0126',
+			email: 'marvin@tesla.com',
+			country: 'Iran',
+			status: 'Active',
+		},
+		{
+			customerName: 'Jerome Bell',
+			company: 'Google.',
+			phoneNumber: '(629) 555-0129',
+			email: 'jerome@google.com',
+			country: 'Réunion',
+			status: 'Active',
+		},
+		{
+			customerName: 'Kathryn Murphy',
+			company: 'Microsoft',
+			phoneNumber: '(406) 555-0120',
+			email: 'kathryn@microsoft.com',
+			country: 'Curaçao',
+			status: 'Active',
+		},
+		{
+			customerName: 'Jacob Jones',
+			company: 'Yahoo',
+			phoneNumber: '(208) 555-0112',
+			email: 'jacob@yahoo.com',
+			country: 'Brazil',
+			status: 'Active',
+		},
+		{
+			customerName: 'Kristin Watson',
+			company: 'Facebook',
+			phoneNumber: '(704) 555-0127',
+			email: 'kristin@facebook.com',
+			country: 'Åland Islands',
+			status: 'Inactive',
+		},
+	];
 
 	// adding an active class to a list item
 	const listItems = document.querySelectorAll('.sidebar__nav-list-item');
